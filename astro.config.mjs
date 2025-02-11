67be6db9755856549e1c13e36d9dffc0be29940b
Johan Ledoux <johan.ledoux25@gmail.com>
2025-02-11 22:50:57 +0100

    Correction + Ajout du pointer sur les btn

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
+    site: 'https://nexusesgi.fr',
+    integrations: [react(), sitemap()],
     vite: {
       plugins: [tailwindcss()],
     },
